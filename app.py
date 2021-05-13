b3f31d7ad943ed8daac5ed2a227c8923aed9325e
Jonathan CHang <jonathan34c@gmail.com>
2021-05-13 22:05:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -68,14 +68,14 @@ def handle_message(event):
 
         else:
             
-            cell = sh.find(event.message.text)
-            if(len(cell)==0):
+            cells = sh.find(event.message.text)
+            if(len(cells)==0):
                 line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text = "抱歉我不懂您的問題，更多婚禮資訊跟功能會在之後推出，如果緊急的話歡迎直接聯絡我們喔！謝謝．")
               )    
             else:
-              print(cell.row) 
+              print(cells[0].address) 
 
     # # Send To Line
     # reply = TextSendMessage(text=f"我的回話是:{get_message}")

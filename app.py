1a727c74a69250989d3dc6654f3da8451ded5110
Jonathan CHang <jonathan34c@gmail.com>
2021-05-13 22:15:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -74,8 +74,7 @@ def handle_message(event):
                 TextSendMessage(text = "抱歉我不懂您的問題，更多婚禮資訊跟功能會在之後推出，如果緊急的話歡迎直接聯絡我們喔！謝謝．")
               )    
             else:
-              cell = cells[0]
-              print(cell.values()) 
+              print(cells[0][0]) 
 
     # # Send To Line
     # reply = TextSendMessage(text=f"我的回話是:{get_message}")
